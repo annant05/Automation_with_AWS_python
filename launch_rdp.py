8f5566444783f512161c7db849d661dcc96a9e0c
Annant05 <annantguptaneema@gmail.com>
2018-08-25 13:52:02 +0530

    updated with option to start the instance

--- launch_rdp.py
+++ launch_rdp.py
@@ -1,29 +1,69 @@
-import os
 import subprocess
 import json
+import os
+import sys
+import time
 
-instance_id = "i-0463*****"
-cmd = "aws ec2 describe-instances --instance-ids " + instance_id
-server_ip = "0.0.0.0"
-user = str(input("enter username : ")).lstrip().rstrip().strip(" ") or "Administrator"
-password = str(input("enter password: ")).lstrip().rstrip().strip(" ") or "password"
+public_ip_add_string = "PublicIpAddress"
 port = "3389"
+user = str(raw_input("enter username: ")).lstrip().rstrip().strip(" ") or "Administrator"
+password = str(raw_input("enter password: ")).lstrip().rstrip().strip(" ") or "9806247089"
 
-response = str(subprocess.check_output(cmd, stderr=subprocess.STDOUT)).replace("\\r\\n", '').strip('b\'')
-# print(response)
-public_ip_add = "PublicIpAddress"
-is_having = (response.find(public_ip_add))
 
-if is_having == -1:
-    print("ec2 instance has no public ipv4")
-    exit(0)
-else:
-    obj_json = json.loads(response)
-    server_ip = str(((obj_json['Reservations'][0])['Instances'][0])[public_ip_add])
-    print(server_ip)
-    cmd_key_cmd = "cmdkey " + "/add:" + server_ip + " /user:" + user + " /pass:" + password
-    mstsc_cmd = "mstsc /v:" + server_ip + ":" + port + " /f"
-    print(cmd_key_cmd)
-    subprocess.check_output(cmd_key_cmd, stderr=subprocess.STDOUT)
-    print(mstsc_cmd)
-    subprocess.check_output(mstsc_cmd, stderr=subprocess.STDOUT)
+def write_response_in_file():
+    global response
+    print(os.getcwd() + "\\response.json")
+    res_file = open("response.json", "w")
+    res_file.write(response)
+
+
+def start_rdp():
+    global response
+    is_having_ip = (response.find(public_ip_add_string))
+    if is_having_ip == -1:
+        print("ec2 instance has no public ipv4")
+        exit(0)
+    else:
+        server_ip = str(instance_data[public_ip_add_string])
+        print(server_ip)
+        cmd_cmdkey = "cmdkey " + "/add:" + server_ip + " /user:" + user + " /pass:" + password
+        cmd_mstsc = "mstsc /v:" + server_ip + ":" + port + " /f"
+        print(cmd_cmdkey)
+        subprocess.Popen(cmd_cmdkey)
+        print(cmd_mstsc)
+        subprocess.Popen(cmd_mstsc)
+        sys.exit(0)
+
+
+def start_instance():
+    cmd_start_instance = "aws ec2 start-instances --instance-ids " + instance_id
+    current_state = subprocess.check_output(cmd_start_instance, stderr=subprocess.STDOUT)
+    print(current_state)
+
+
+def check_state():
+    global state
+    if state == "stopped":
+        choice = raw_input("Enter \"YES\" to start the instance:  ")
+        if choice == "YES":
+            start_instance()
+            wait = 60
+            print('Please! wait for ' + str(wait) + 'seconds')
+            time.sleep(wait)
+            check_state()
+        else:
+            print("BYE :)")
+            exit(0)
+    else:
+        start_rdp()
+
+
+instance_id = "i-0d212a70a060e0ba7"
+cmd_describe_ec2 = "aws ec2 describe-instances --instance-ids " + instance_id
+
+response = str(subprocess.check_output(cmd_describe_ec2, stderr=subprocess.STDOUT)).replace("\\r\\n", '').strip('b\'')
+obj_json = json.loads(response)
+
+instance_data = ((obj_json['Reservations'][0])['Instances'][0])
+state = str((instance_data['State'])['Name'])
+check_state()
