a41101a45b4c07587a639860d41910a46268040e
annant <annantguptaneema@gmail.com>
2018-12-20 20:36:07 +0530

    added a few comments. optimization in  function detach_attach_volume()

--- launch_rdp.py
+++ launch_rdp.py
@@ -4,52 +4,95 @@ import os
 import sys
 import time
 
+# Dictionary of instances and their types for easy editing
 instance = {
-    "t2.micro": "i-",
-    "t2.small": "i-",
-    "t2.medium": "i-"
+    "t2.micro": "i-027b481b0af2a79ae",
+    "t2.small": "i-0f1f555ed44e3fbdd",
+    "t2.medium": "i-0242a6d8d6acd36bf"
 }
 
-instance_id = ""
-public_ip_add_string = "PublicIpAddress"
+# Dictionary of volumes with their attachment_type : For ex: /dev/sda1
+volumes = {
+    "main_boot_vol": ["vol-0bf263386c4415c6f", "/dev/sda1"],
+    "additional_storage_vol": ["vol-05a99df42b26e6b83", "/dev/sda1"],
+    "temp_5gb_vol": ["vol-02a6bf5074d93f6f1", "xvdf"]
+}
+
+# enter the username and password used to login into the instance using RDP.
+user = ""
+password = ""
+
+# Change if using any custom port for RDP.
 port = "3389"
 
-user = str(input("enter username : ")).lstrip().rstrip().strip(" ") or "Administrator"
-password = str(input("enter password: ")).lstrip().rstrip().strip(" ") or ""
+# This variable is used to set instance_id to be used by all the functions.
+instance_id = ""
 
+# This string is searched in JSON response to get the Public IP_address of the instance.
+public_ip_add_string = "PublicIpAddress"
+
+# JSON response from the ec2 describe function is stored in this variable for use by different functions.
 response = ""
+
+# JSON response is traversed to get description data of the chosen instance.
 instance_data = ""
+
+# Get the state of the instance to perform the respective function or operation.
 state = ""
-volume_id = "vol-"
 
 
+# Function that is used to select the instance for launch or RDP.
 def select_instance():
-    ch = str(raw_input(" f: t2.micro(free)  /  s:  t2.small(economy)   /  m: t2.medium(performance) : "))
+    # Show the list of available instances.
+    ch = str(input(" f: t2.micro(free)  /  s:  t2.small(economy)   /  m: t2.medium(performance) : "))
+
+    # access global variable instance_id inside the function
     global instance_id
+
+    # if else code to change global instance id to the selected instance id.
     if ch == 'f':
-        instance_id = instance["t2.micro"]  # instance id for t2.small
+        instance_id = instance["t2.micro"]
     elif ch == 'm':
-        instance_id = instance["t2.medium"]  # instance id for t2.medium
+        instance_id = instance["t2.medium"]
     elif ch == 's':
-        instance_id = instance["t2.small"]  # instance id for t2.small
+        instance_id = instance["t2.small"]
     else:
-        print("Wrong option selected  ")
+        print("Wrong option selected. \n Enter a valid choice ")
+        # If wrong option is selected. Ask again for a valid choice recursively.
         select_instance()
-    print("Initial Instance id : " + instance_id)
+
+    # print the selected instance to the command line
+    print(" You have selected Instance with id : " + instance_id)
+
+    # Call function to check state of the instance.
     check_state()
 
 
+# Function that checks the instance state to decide between RDP or starting the instance.
 def check_state():
+    # access global variables instance_data and state inside the function.
     global instance_data
     global state
+
+    # amount of time to wait between checking for change in the state of the instance.
     wait = 30
+
+    # call this function to execute the aws cli commands.
     get_instance_state()
+
+    # This conditional statements decide to  RDP or start the instance.
+
+    # if the instance is started and in pending state then wait for some time and check again.
     if state == "pending":
         time.sleep(wait)
         check_state()
+
+    # if the instance is stooped then ask the user to start it and proceed.
     elif state == "stopped":
-        choice = raw_input("Enter \"YES\" to start the instance:  ")
-        if choice == "YES":
+
+        # get user input from command line
+        choice = input("Enter \"y\" to start the instance:  ")
+        if choice == "y":
             detach_attach_volume(instance_id)
             start_instance()
             print('Please! wait for ' + str(wait) + 'seconds')
@@ -60,7 +103,7 @@ def check_state():
             exit(0)
     else:
         start_rdp()
-        print ("instance started" + instance_id)
+        print("instance started" + instance_id)
 
 
 def get_instance_state():
@@ -68,44 +111,14 @@ def get_instance_state():
     global instance_data
     global state
     cmd_describe_ec2 = "aws ec2 describe-instances --instance-ids " + instance_id
-
     try:
-        response = str(subprocess.check_output(cmd_describe_ec2, stderr=subprocess.STDOUT)).replace("\\r\\n", '').strip(
-            'b\'')
+        response = str(subprocess.check_output(cmd_describe_ec2, stderr=subprocess.STDOUT))
         obj_json = json.loads(response)
         instance_data = ((obj_json['Reservations'][0])['Instances'][0])
         state = str((instance_data['State'])['Name'])
 
     except subprocess.CalledProcessError as e:
-        print "Error describing instance : ", e.output
-
-
-def detach_attach_volume(instance_to_attach):
-    global volume_id
-    cmd_detach_volume = "aws ec2 detach-volume --volume-id " + volume_id
-
-    try:
-        print(str(subprocess.check_output(cmd_detach_volume, stderr=subprocess.STDOUT)).replace("\\r\\n", '').strip(
-            'b\''))
-    except subprocess.CalledProcessError as e:
-        print "Already attached: ", e.output
-
-    cmd_attach_volume = "aws ec2 attach-volume --volume-id " \
-                        + volume_id + " --device /dev/sda1 --instance " + instance_to_attach
-    print (str(subprocess.check_output(cmd_attach_volume, stderr=subprocess.STDOUT)).replace("\\r\\n", '').strip(
-        'b\''))
-
-
-def start_instance():
-    global instance_id
-    print("Instance id in start_instance is : " + instance_id)
-    cmd_start_instance = "aws ec2 start-instances --instance-ids " + instance_id
-    try:
-        current_state = subprocess.check_output(cmd_start_instance, stderr=subprocess.STDOUT)
-        print(current_state)
-    except subprocess.CalledProcessError as e:
-        print "Error starting instance : ", e.output
-        exit()
+        print("Error describing instance : ", e.output)
 
 
 def start_rdp():
@@ -127,9 +140,41 @@ def start_rdp():
             subprocess.Popen(cmd_mstsc)
             sys.exit(0)
         except subprocess.CalledProcessError as e:
-            print "Error: ", e.output
+            print("Error: ", e.output)
 
 
+def start_instance():
+    global instance_id
+    print("Instance id in start_instance is : " + instance_id)
+    cmd_start_instance = "aws ec2 start-instances --instance-ids " + instance_id
+    try:
+        current_state = subprocess.check_output(cmd_start_instance, stderr=subprocess.STDOUT)
+        print(current_state)
+    except subprocess.CalledProcessError as e:
+        print("Error starting instance : ", e.output)
+        exit()
+
+
+def detach_attach_volume(instance_to_attach):
+    global volumes
+    cmd_detach_volume = "aws ec2 detach-volume --volume-id "
+
+    for vol in volumes.keys():
+        try:
+            print(str(subprocess.check_output(cmd_detach_volume + volumes[vol][0], stderr=subprocess.STDOUT)))
+        except subprocess.CalledProcessError as e:
+            print("Already attached: ", e.output)
+
+    for vol in volumes.keys():
+        try:
+            cmd_attach_volume = "aws ec2 attach-volume --volume-id " + volumes[vol][0] + " --device " + volumes[vol][
+                1] + " --instance " + instance_to_attach
+            print(str(subprocess.check_output(cmd_attach_volume, stderr=subprocess.STDOUT)))
+        except subprocess.CalledProcessError as e:
+            print("Error attaching volume: ", e.output)
+
+
+# Function to write the instance data in a file for debugging.
 def write_response_in_file():
     global response
     print(os.getcwd() + "\\response.json")
